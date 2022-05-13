6990545ce8fd517504aa64295cbefac476565920
Sam Molyneux <sam@molyneuxs.net>
2022-05-13 14:59:01 +0100

    added SUMO tools

--- Simulation/simple/basic/commands.bash
+++ Simulation/simple/basic/commands.bash
@@ -5,43 +5,29 @@ do
     esac
 done
 
-
+#generate origin destination trips
 python ../../tools/randomTrips.py -n basicGrid.net.xml --fringe-factor "$fringeFactor" --junction-taz -o simpleTrips.trips.xml
 
+#generate routes
 duarouter -n basicGrid.net.xml --junction-taz --r simpleTrips.trips.xml -o basicRoutes.rou.xml
 
-#python ../../tools/randomTrips.py -n basicGrid.net.xml --fringe-factor 5 --junction-taz -r basicRoutes.rou.xml
-
+#simulate the routes to for baseline demand
 sumo -c basic_config_18_04_2022.sumocfg
 
-# <configuration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/sumoConfiguration.xsd">
-
-#     <input>
-#         <net-file value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\basicGrid.net.xml"/>
-#         <route-files value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\basicRoutes.rou.xml"/>
-#         <additional-files value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\edgeDataDef.xml" />
-#     </input>
-
-# </configuration>
-
+#scale demand to match count data
 python ../../mytools/scaleDemand.py --edgedata-file edgeData.xml --count-file countData.xml -o expandedCounts.xml
 
+#use scaled count data as input for route sampling
 python ../../tools/routeSampler.py -r basicRoutes.rou.xml -d expandedCounts.xml -o sampledRoutes.rou.xml 
 
+#run baseline demand simulation
 sumo -c analysis_config.sumocfg
 
-# <configuration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/sumoConfiguration.xsd">
-
-#     <input>
-#         <net-file value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\basicGrid.net.xml"/>
-#         <route-files value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\sampledRoutes.rou.xml"/>
-#         <additional-files value="C:\Users\sam\Documents\Work\Final%20Year\Dissertation\diss\Simulation\simple\basic\analysisDataDef.xml" />
-#     </input>
-
-# </configuration>
-
+#generate trips with correct volume from the baseline demand 
 python ../../mytools/tripsFromRoutes.py  -i sampledRoutes.rou.xml -r basicRoutes.rou.xml -t simpleTrips.trips.xml -o ../ltn/ltnTrips.rou.xml
 
+#route the trips on the converted network
 duarouter -n ../ltn/ltnGrid.net.xml --r ../ltn/ltnTrips.rou.xml -o ../ltn/ltnRoutes.rou.xml --junction-taz
 
+#run the simulation of the converted network
 sumo -c ../ltn/ltn_config.sumocfg
